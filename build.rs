60966202fcf3cb8dabf89b7206c768852cb74ff7
rob <roby.pakalns@gmail.com>
2025-04-11 20:00:58 +0300

    dist

--- build.rs
+++ build.rs
@@ -1,18 +1,31 @@
-use std::process::exit;
+use std::process::{Command, exit};
 use winres::WindowsResource;
 
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = WindowsResource::new();
-        res.set("ProductName", "Tricko Client")
-            .set("FileDescription", "Tricko Client")
-            .set_icon("./frontend/icon.ico")
-            .set("ProductVersion", env!("CARGO_PKG_VERSION"))
-            .set("FileVersion", env!("CARGO_PKG_VERSION"));
+    if !cfg!(target_os = "windows") {
+        return;
+    }
 
-        if let Err(e) = res.compile() {
-            eprintln!("{}", e);
-            exit(1);
-        }
+    let mut res = WindowsResource::new();
+    res.set("ProductName", "Tricko Client")
+        .set("FileDescription", "Tricko Client")
+        .set_icon("./frontend/icon.ico")
+        .set("ProductVersion", env!("CARGO_PKG_VERSION"))
+        .set("FileVersion", env!("CARGO_PKG_VERSION"));
+
+    if let Err(_) = res.compile() {
+        exit(1);
+    }
+
+    let output = Command::new("wix")
+        .arg("build")
+        .arg("build.wxs")
+        .arg("-out")
+        .arg("./dist/tricko-client-win.msi")
+        .output()
+        .expect("Failed to execute wix build");
+
+    if !output.status.success() {
+        exit(1);
     }
 }
