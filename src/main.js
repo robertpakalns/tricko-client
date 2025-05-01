150c8a7062d4ea6dbd3db9591faf17a20f227733
rob <roby.pakalns@gmail.com>
2025-05-01 16:51:38 +0300

    api cors fix for dev

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import.meta.hot?.accept()
+// DEV
+const _fetch = window.fetch
+window.fetch = function (...args) {
+    const [url, ...a] = args
+    if (url.startsWith("https://api.tricko.pro")) return _fetch(url.replace("https://api.tricko.pro", "/api"), ...a)
+    return _fetch(...args)
+}
 
-
-// main code
+import { invoke } from '@tauri-apps/api/core'
 console.log("injected!!!!")
 document.querySelectorAll("h2").forEach(el => el.style.color = "lime")
+document.addEventListener("keyup", async e => {
+    if (e.key === "F11") {
+        e.preventDefault()
+        await invoke("toggle_fullscreen")
+    }
+})
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,33 @@
 import { defineConfig } from "vite"
 
 const domain = "https://tricko.pro"
+const apiDomain = "https://api.tricko.pro"
 const injectedScript = '<script type="module" src="/src/main.js"></script>'
 
 export default defineConfig({
     server: {
         proxy: {
             "/assets/": domain,
-            "/": {
-                target: domain,
-                changeOrigin: true
+            "/api/": {
+                target: apiDomain,
+                changeOrigin: true,
+                rewrite: path => path.replace("/api", "")
             },
             "^/(?!@vite|src|node_modules|\.vite).*": {
                 target: domain,
                 changeOrigin: true,
+                ws: true,
                 selfHandleResponse: true,
                 bypass: (req, res) => {
-                    if (!req.headers.accept?.includes("text/html")) return req.url
-                    return fetch(domain)
+                    if (req.headers.accept?.includes("text/html")) return fetch(domain)
                         .then(r => r.text())
                         .then(html => {
                             res.setHeader("content-type", "text/html")
                             res.end(html.replace("</body>", `${injectedScript}</body>`))
                             return true
                         })
+
+                    return req.url
                 }
             }
         }
